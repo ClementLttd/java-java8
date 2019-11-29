37e56ab8707dc5479039b3ec68f61907ce4d8c96
Clement Littardi <littardi.clement@gmail.com>
2019-11-29 16:43:39 +0100

    2e commit

--- src/test/java/java8/ex01/DateAndTime_01_Test.java
+++ src/test/java/java8/ex01/DateAndTime_01_Test.java
@@ -18,14 +18,15 @@ public class DateAndTime_01_Test {
     public void test_date() {
 
         // TODO modifier les paramètres pour que le test soit passant
-        Date date = new Date(0, 0, 0, 0,0,0);
+        Date date = new Date(117, 2, 24, 1,2,3);
 
         assertThat(date.toString(), is("Fri Mar 24 01:02:03 CET 2017"));
 
         // une date est mutable... :-(
         // TODO modifier le jour de la date pour que le test soit passant
-
-        assertThat(date.toString(), is("Sat Mar 25 01:02:03 CET 2017"));
+        Date date1 = new Date(117, 2, 25, 1,2,3);
+        
+        assertThat(date1.toString(), is("Sat Mar 25 01:02:03 CET 2017"));
     }
 
     // Vous vous souvenez de java.util.Calendar (Java 1.1) ?
